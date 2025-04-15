003ec3975b2172771ac924d7a339495616aaf774
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-15 23:25:03 +0530

    Navbar ui is completed in mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import { FaBars } from "react-icons/fa6";
 const Navbar = () => {
     return (
-        <div className='w-full p-4 bg-black text-white'>
+        <div className='w-full p-4 flex items-center justify-between bg-black text-white'>
             <img src="https://www.ayusynk.ai/wp-content/uploads/2024/07/logo-1.png" alt="" />
 
-            <div className='icons'>
-                <a href="">
-                    <img src="https://www.ayusynk.ai/wp-content/uploads/2024/07/shopping-bag-1.png" alt="" />
+            <div className='icons flex items-center gap-4'>
+                <a className='' href="">
+                    <img  src="https://www.ayusynk.ai/wp-content/uploads/2024/07/shopping-bag-1.png" alt="" />
                 </a>
-                
+                <FaBars className='text-xl' />
             </div>
         </div>
     )
