258214372777c2fbed5005926a7f2eb712f5d72b
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-16 22:33:06 +0530

    Ehance section text is updated

--- src/components/Enhance.jsx
+++ src/components/Enhance.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Enhance = () => {
   return (
-    <div className='w-full px-4 py-5 bg-[#EEEEEE]'>
-        <h1>Enhancing precision and accuracy in healthcare by syncing digital technology for a better healthcare</h1>
-        <p>We strive to improve health and extend lives by blending technology with medicine for accurate diagnosis and monitoring. Our mission is to offer innovative healthcare products and exceptional service to enhance healthcare outcomes.</p>
+    <div className='w-full px-4 py-10 bg-[#EEEEEE]'>
+        <h1 className='text-[1.55em] font-semibold text-center'>Enhancing precision and accuracy in healthcare by syncing digital technology for a better healthcare</h1>
+        <p className='text-[1em] text-center pt-2 leading-[6vw]'>We strive to improve health and extend lives by blending technology with medicine for accurate diagnosis and monitoring. Our mission is to offer innovative healthcare products and exceptional service to enhance healthcare outcomes.</p>
     </div>
   )
 }
