a8549d20e73484d7e6b1796121660fea4cef242c
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-16 22:42:54 +0530

    no number stats ui is also completed for mobile screens

--- src/components/Enhance.jsx
+++ src/components/Enhance.jsx
@@ -5,6 +5,20 @@ const Enhance = () => {
     <div className='w-full px-4 py-10 bg-[#EEEEEE]'>
         <h1 className='text-[1.55em] font-semibold text-center'>Enhancing precision and accuracy in healthcare by syncing digital technology for a better healthcare</h1>
         <p className='text-[1em] text-center pt-2 leading-[6vw]'>We strive to improve health and extend lives by blending technology with medicine for accurate diagnosis and monitoring. Our mission is to offer innovative healthcare products and exceptional service to enhance healthcare outcomes.</p>
+        <div className='numbers pt-10 flex items-center justify-center gap-10 flex-wrap '>
+            <div className='sold '>
+                <h1 className='text-[2.4em] font-bold text-[rgb(56, 56, 56)]'>10000+</h1>
+                <p className='text-center'>Devices Sold</p>
+            </div>
+            <div className='sold '>
+                <h1 className='text-[2.4em] font-bold text-[rgb(56, 56, 56)]'>67822+</h1>
+                <p className='text-center'>Patients screened</p>
+            </div>
+            <div className='sold'>
+                <h1 className='text-[2.4em] font-bold text-[rgb(56, 56, 56)]'>300000+</h1>
+                <p className='text-center'>No. of recordings</p>
+            </div>
+        </div>
     </div>
   )
 }
