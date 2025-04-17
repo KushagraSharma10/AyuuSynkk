7343f1cbe698557c21bea687eed447903ab9794f
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-17 17:12:19 +0530

    Sync ui is made responsive for both mobile and laptop

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Enhance from './components/Enhance'
+import Sync from './components/Sync'
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <Navbar />
       <Hero />
       <Enhance />
+      <Sync />
+
     </div>
   )
 }
---
+++ src/components/Sync.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Sync = () => {
+  return (
+    <div className='w-full px-4 lg:px-[5vw] py-10 lg:py-15 bg-black'>
+        <h1 className='lg:text-[3.1em] lg:font-semibold lg:w-[60vw] lg:mx-auto  lg:text-center text-center text-[1.9em] text-[#DEDEDE] font-medium'>Sync digital technology for a  superior healthcare experience</h1>
+
+        <div className='cards flex items-center gap-10 lg:gap-6 mt-10 w-[300vw]'>
+            <div className='card w-[93vw] h-[90vw] border border-white flex items-center justify-center  rounded-[3vw] border-none bg-[#1263CD] lg:w-[28vw] lg:h-[35vw] lg:rounded-[1vw]'>
+                <img className='w-[60vw] h-[80vw] lg:rounded-[0vw] lg:w-[37vw] lg:h-full object-cover' src="https://www.ayusynk.ai/wp-content/uploads/2024/08/st-a2.png" alt="" />
+            </div>
+            <div className='card w-[93vw] h-[90vw] border border-white flex items-center justify-center  rounded-[3vw] border-none bg-[#1263CD] lg:w-[28vw] lg:h-[35vw] lg:rounded-[1vw]'>
+                <img className='w-[60vw] h-[80vw] lg:rounded-[0vw] lg:w-[37vw] lg:h-full object-cover' src="https://www.ayusynk.ai/wp-content/uploads/2024/08/st-a2.png" alt="" />
+            </div>
+            <div className='card w-[93vw] h-[90vw] border border-white flex items-center justify-center  rounded-[3vw] border-none bg-[#1263CD] lg:w-[28vw] lg:h-[35vw] lg:rounded-[1vw]'>
+                <img className='w-[60vw] h-[80vw] lg:rounded-[0vw] lg:w-[37vw] lg:h-full object-cover' src="https://www.ayusynk.ai/wp-content/uploads/2024/08/st-a2.png" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sync
