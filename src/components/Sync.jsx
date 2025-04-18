1551b99ab59724e0ed1c51060463bb074cfb175b
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-18 23:59:17 +0530

    functionality is updated a bit

--- src/components/Sync.jsx
+++ src/components/Sync.jsx
@@ -4,6 +4,8 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const Sync = () => {
 
+  const cards = document.querySelector('.gsap-cards')
+
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
 
@@ -12,11 +14,13 @@ const Sync = () => {
       x: () => -(document.querySelector('.gsap-cards').scrollWidth - window.innerWidth),
       scrollTrigger: {
         trigger: ".gsap-section",
-        start: "top top",
-        end: () => "+=" + (document.querySelector('.gsap-cards').scrollWidth * 0.8),
+        start: "top center",
+        end: () => "+=" + (cards.scrollWidth * 1.5), // 👈 Dynamic end point
         scrub: 1,
         pin: true,
-        invalidateOnRefresh: true // Responsive ke liye important
+        invalidateOnRefresh: true, // Responsive ke liye important
+        markers: true, // Position debug karein
+      pinSpacing: false // 👈 Extra space hatane ke liye
       }
     })
 
@@ -27,12 +31,12 @@ const Sync = () => {
   }, [])
 
   return (
-    <div className='gsap-section w-full px-4 lg:px-[5vw] py-10 lg:py-15 bg-black overflow-x-hidden'>
+    <div className='gsap-section w-full px-4 lg:px-[5vw] py-10 lg:py-15 bg-black overflow-x-hidden min-h-[60vh]'>
       <h1 className='lg:text-[3.1em] lg:font-semibold lg:w-[60vw] lg:mx-auto  lg:text-center text-center text-[1.9em] text-[#DEDEDE] font-medium'>
         Sync digital technology for a superior healthcare experience
       </h1>
 
-      <div className='gsap-cards flex items-center gap-10 lg:gap-6 mt-10 lg:w-[100vw] w-[300vw] min-h-[80vh]'>
+      <div className='gsap-cards flex items-center gap-10 lg:gap-6 mt-10 lg:w-[100vw] w-[300vw] '>
         <div className='card w-[93vw] h-[90vw] border border-white flex items-center flex-shrink-0 lg:flex-shrink-0 justify-center  rounded-[3vw] border-none bg-[#1263CD] lg:w-[28vw] lg:h-auto lg:rounded-[1vw]'>
           <img className='w-[60vw] h-[80vw] lg:rounded-[0vw] lg:w-[37vw] lg:h-full object-cover' src="https://www.ayusynk.ai/wp-content/uploads/2024/08/st-a2.png" alt="" />
 
