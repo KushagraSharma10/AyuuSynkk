507a9f502775528029cf2bd2cadb3389901d1aa2
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-19 00:42:59 +0530

    TextSection is completed for mobile screen of RealTime Section

--- src/components/RealTime.jsx
+++ src/components/RealTime.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const RealTime = () => {
   return (
-    <div className='bg-red-600'>RealTime</div>
+    <div className='bg-[#EAEAEA] px-4 py-11' >
+        <div className='Text-Area'>
+            <h1 className='text-[1.55em] lg:text-[2.4em] font-bold leading-8'>Real-Time monitoring for enhanced diagnostics.</h1>
+            <p className='font-medium pt-2 leading-[6vw]'>The unified ayu dashboard ecosystem provides access to real-time monitoring data. The technology is designed to provide with dedicated filter for clarity, precision and accuracy. AyuSynk features one tap connectivity with smartphones using bluetooth technology, guided capturing of physiological signals and web streaming of vital signs.</p>
+           
+        </div>
+        <hr className='text-[#BEBEBE] mt-5' />
+    </div>
   )
 }
 
