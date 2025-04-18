ce58cb7a34d53f594a34d971c17a586fd34f7604
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-18 23:23:26 +0530

    gsap's property scrollTrigger is applied to cards section to make it pin and apply beautiful feature

--- src/components/Sync.jsx
+++ src/components/Sync.jsx
@@ -2,27 +2,29 @@ import React, { useEffect } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(ScrollTrigger)
-
-function gsapAnimation() {
-  gsap.to(".gsap-cards", {
-    x: "-210%",
-    scrollTrigger: {
-      trigger: ".gsap-section",
-      scroller: "body", // ya '#main' if you have custom scroll container
-      start: "top top",
-      end: "top -100%",
-      scrub: 2,
-      pin: true,
-      // markers: true,
-    },
-  });
-}
-
 const Sync = () => {
+
   useEffect(() => {
-    gsapAnimation();
-  }, []);
+    gsap.registerPlugin(ScrollTrigger)
+
+    // Simple animation with class selectors
+    gsap.to(".gsap-cards", {
+      x: "-210%",
+      scrollTrigger: {
+        trigger: ".gsap-section",
+        start: "top top",
+        end: "top -150%",
+        scrub: 2,
+        pin: true,
+        markers: true // Debug ke liye
+      },
+    })
+
+    // Cleanup
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill())
+    }
+  }, [])
 
   return (
     <div className='gsap-section w-full px-4 lg:px-[5vw] py-10 lg:py-15 bg-black'>
@@ -30,12 +32,14 @@ const Sync = () => {
         Sync digital technology for a superior healthcare experience
       </h1>
 
-      <div className='gsap-cards flex items-center gap-10 lg:gap-6 mt-10 w-[1000vw]'>
+      <div className='gsap-cards flex items-center gap-10 lg:gap-6 mt-10 lg:w-[300vw] w-[1000vw]'>
         <div className='card w-[93vw] h-[90vw] border border-white flex items-center justify-center  rounded-[3vw] border-none bg-[#1263CD] lg:w-[28vw] lg:h-[35vw] lg:rounded-[1vw]'>
           <img className='w-[60vw] h-[80vw] lg:rounded-[0vw] lg:w-[37vw] lg:h-full object-cover' src="https://www.ayusynk.ai/wp-content/uploads/2024/08/st-a2.png" alt="" />
+
         </div>
         <div className='card w-[93vw] h-[90vw] border border-white flex items-center justify-center  rounded-[3vw] border-none bg-[#1263CD] lg:w-[28vw] lg:h-[35vw] lg:rounded-[1vw]'>
           <img className='w-[60vw] h-[80vw] lg:rounded-[0vw] lg:w-[37vw] lg:h-full object-cover' src="https://www.ayusynk.ai/wp-content/uploads/2024/08/st-a2.png" alt="" />
+          
         </div>
         <div className='card w-[93vw] h-[90vw] border border-white flex items-center justify-center  rounded-[3vw] border-none bg-[#1263CD] lg:w-[28vw] lg:h-[35vw] lg:rounded-[1vw]'>
           <img className='w-[60vw] h-[80vw] lg:rounded-[0vw] lg:w-[37vw] lg:h-full object-cover' src="https://www.ayusynk.ai/wp-content/uploads/2024/08/st-a2.png" alt="" />
