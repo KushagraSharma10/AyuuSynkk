9476b1e38220cf657beb026977477104bec61ab1
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-19 00:51:40 +0530

    RealTime section is updated

--- src/components/RealTime.jsx
+++ src/components/RealTime.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const RealTime = () => {
   return (
-    <div className='bg-[#EAEAEA] px-4 py-11' >
-        <div className='Text-Area'>
-            <h1 className='text-[1.55em] lg:text-[2.4em] font-bold leading-8'>Real-Time monitoring for enhanced diagnostics.</h1>
-            <p className='font-medium pt-2 leading-[6vw]'>The unified ayu dashboard ecosystem provides access to real-time monitoring data. The technology is designed to provide with dedicated filter for clarity, precision and accuracy. AyuSynk features one tap connectivity with smartphones using bluetooth technology, guided capturing of physiological signals and web streaming of vital signs.</p>
+    <div className='bg-[#EAEAEA] px-4 py-11 lg:px-[5vw]' >
+        <div className='Text-Area lg:flex lg:items-center lg:gap-[15vw]'>
+            <h1 className='text-[1.55em] lg:text-[2.4em] font-bold leading-8 lg:w-[30vw] lg:leading-13'>Real-Time monitoring for enhanced diagnostics.</h1>
+            <p className='font-medium pt-2 leading-[6vw] lg:leading-[1.5vw] lg:w-[40vw] lg:text-[1.1em]'>The unified ayu dashboard ecosystem provides access to real-time monitoring data. The technology is designed to provide with dedicated filter for clarity, precision and accuracy. AyuSynk features one tap connectivity with smartphones using bluetooth technology, guided capturing of physiological signals and web streaming of vital signs.</p>
            
         </div>
         <hr className='text-[#BEBEBE] mt-5' />
