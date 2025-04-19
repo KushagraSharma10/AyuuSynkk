4ebd290e90a171b00c6034192151a22a3c57c376
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-19 23:54:09 +0530

    marquee section is updated with some changes

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 
 const Marquee = () => {
-  const Images = [
-    "https://www.ayusynk.ai/wp-content/uploads/2024/09/medongo.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/call-health.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/apollo.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/blossom.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/medtel.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/yolohealth.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/curebay.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/09/medongo.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/call-health.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/apollo.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/blossom.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/medtel.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/yolohealth.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/curebay.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/09/medongo.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/call-health.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/apollo.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/blossom.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/medtel.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/yolohealth.svg",
-    "https://www.ayusynk.ai/wp-content/uploads/2024/07/curebay.svg",
-  ];
+    const images = [
+        "https://www.ayusynk.ai/wp-content/uploads/2024/09/medongo.svg",
+        "https://www.ayusynk.ai/wp-content/uploads/2024/07/call-health.svg",
+        "https://www.ayusynk.ai/wp-content/uploads/2024/07/apollo.svg",
+        "https://www.ayusynk.ai/wp-content/uploads/2024/07/blossom.svg",
+        "https://www.ayusynk.ai/wp-content/uploads/2024/07/medtel.svg",
+        "https://www.ayusynk.ai/wp-content/uploads/2024/07/yolohealth.svg",
+        "https://www.ayusynk.ai/wp-content/uploads/2024/07/curebay.svg",
+      ];
+    
+      const duplicatedImages = [...images, ...images]; // Duplicate for seamless loop
 
   return (
     <div className="w-full p-4 py-10 overflow-hidden bg-[#1367D5]">
@@ -32,7 +20,7 @@ const Marquee = () => {
       </h1>
       <div className="marquee flex items-center gap-[1vw] w-[500vw] lg:w-[100vw] mt-5 animate-marquee">
         <div className="flex items-center gap-[2vw]">
-            {Images.map((img, index)=>{
+            {duplicatedImages.map((img, index)=>{
                 return(
                     <img
                     key={index}
