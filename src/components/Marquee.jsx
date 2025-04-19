72ec044992e0b365d663a21f9b176f9422f4aa48
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-19 23:50:37 +0530

    marquee animation is updated

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -16,6 +16,13 @@ const Marquee = () => {
     "https://www.ayusynk.ai/wp-content/uploads/2024/07/medtel.svg",
     "https://www.ayusynk.ai/wp-content/uploads/2024/07/yolohealth.svg",
     "https://www.ayusynk.ai/wp-content/uploads/2024/07/curebay.svg",
+    "https://www.ayusynk.ai/wp-content/uploads/2024/09/medongo.svg",
+    "https://www.ayusynk.ai/wp-content/uploads/2024/07/call-health.svg",
+    "https://www.ayusynk.ai/wp-content/uploads/2024/07/apollo.svg",
+    "https://www.ayusynk.ai/wp-content/uploads/2024/07/blossom.svg",
+    "https://www.ayusynk.ai/wp-content/uploads/2024/07/medtel.svg",
+    "https://www.ayusynk.ai/wp-content/uploads/2024/07/yolohealth.svg",
+    "https://www.ayusynk.ai/wp-content/uploads/2024/07/curebay.svg",
   ];
 
   return (
@@ -23,13 +30,13 @@ const Marquee = () => {
       <h1 className="text-[1.6em] text-center text-white">
         Trusted by health care partners
       </h1>
-      <div className="marquee flex items-center gap-[1vw] w-[500vw] mt-5 animate-marquee">
-        <div className="flex items-center gap-[1vw]">
+      <div className="marquee flex items-center gap-[1vw] w-[500vw] lg:w-[100vw] mt-5 animate-marquee">
+        <div className="flex items-center gap-[2vw]">
             {Images.map((img, index)=>{
                 return(
                     <img
                     key={index}
-                    className="w-[45vw] lg:w-[15vw] h-auto"
+                    className="min-w-[150px] lg:min-w-[200px] h-auto"
                     src={img}
                     alt=""
                 />
