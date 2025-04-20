6c212115a0f378267e753fabebafcd47a1c6ca86
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-21 01:15:28 +0530

    Enhance Section made responsive for tablet screen

--- src/components/Enhance.jsx
+++ src/components/Enhance.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Enhance = () => {
     return (
-        <div className='w-full px-4 py-10 lg:px-[15vw] lg:py-[5.5vw] bg-[#EEEEEE]'>
+        <div className='w-full px-4 py-10 lg:px-[15vw] md:px-[6vw] md:py-[10.5vw] lg:py-[5.5vw] bg-[#EEEEEE]'>
             
-            <h1 className='text-[1.55em] lg:text-[2.4em] font-semibold text-center'>Enhancing precision and accuracy in healthcare by syncing digital technology for a better healthcare</h1>
+            <h1 className='text-[1.55em] md:text-[2.4em] lg:text-[2.4em] font-semibold text-center'>Enhancing precision and accuracy in healthcare by syncing digital technology for a better healthcare</h1>
 
-            <p className='text-[1em] text-center pt-2 leading-[6vw] lg:leading-[2.3vw] lg:text-[1.3em] lg:pt-4'>We strive to improve health and extend lives by blending technology with medicine for accurate diagnosis and monitoring. Our mission is to offer innovative healthcare products and exceptional service to enhance healthcare outcomes.</p>
+            <p className='text-[1em] md:text-[1.1em] md:leading-[4vw] text-center pt-2 leading-[6vw] lg:leading-[2.3vw] lg:text-[1.3em] lg:pt-4'>We strive to improve health and extend lives by blending technology with medicine for accurate diagnosis and monitoring. Our mission is to offer innovative healthcare products and exceptional service to enhance healthcare outcomes.</p>
 
             <div className='numbers pt-10 flex items-center justify-center gap-10 lg:gap-20 lg:pt-15 flex-wrap '>
                 <div className='sold '>
