153ef8436a4fef4e09cb6c0609a13f54fa493876
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-21 00:54:46 +0530

    Navbar section is made responsive for tablet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
     const Links = ["products", "solution", "partner", "Company"]
 
     return (
-        <div className='w-full px-4 lg:px-[5vw] py-6 flex items-center justify-between bg-black text-white'>
+        <div className='w-full px-4 lg:px-[5vw] md:px-[5vw] py-6 flex items-center justify-between bg-black text-white'>
             <img src="https://www.ayusynk.ai/wp-content/uploads/2024/07/logo-1.png" alt="" />
 
-            <div className='links hidden lg:flex lg:gap-10 '>
+            <div className='links hidden lg:flex lg:gap-10'>
                 {Links.map((link, index) =>{
                     return <a className='text-[1.1em] capitalize' key={index} href=''>{link}</a>
                 })}
