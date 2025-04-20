3baf2d2c7949425da1cf8e74a3b137a607336adc
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-21 00:43:25 +0530

    Hero section is updated accoding to Table screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="w-full px-4 py-10 h-[109.91vw] lg:h-[53.9vw] lg:bg-black bg-black relative">
-      <h1 className="lg:absolute lg:top-[18vw] lg:w-[35.5vw] lg:left-[7vw] lg:bg-transparent lg:text-[2.9em] text-[1.8em] text-white font-medium z-2">
+    <div className="w-full px-4  py-10 h-[109.91vw]  lg:h-[53.9vw] md:h-[65vw] lg:bg-black bg-black relative">
+      <h1 className="lg:absolute md:absolute md:top-[18vw] md:left-[6vw] md:leading-[8vw] md:font-medium lg:top-[18vw] md:w-[72vw] lg:w-[35.5vw] lg:left-[7vw] lg:bg-transparent lg:text-[2.9em] md:text-[3em] text-[1.8em] text-white font-medium z-2">
         Experience the future of precision monitoring with{" "}
         <span className="text-[rgb(50,113,195)]">AyuSynk</span>
       </h1>
-      <div className="video-content absolute w-full lg:h-screen lg:top-0 bottom-0 right-0">
+      <div className="video-content absolute w-full md:h-[65vw]  md:top-[0vw] lg:h-screen lg:top-0 bottom-0 right-0">
         <video
-          className="w-full h-[30vh] lg:h-full object-cover"
+          className="w-full h-[30vh] md:h-full lg:h-full object-cover"
           muted
           autoPlay
           loop
